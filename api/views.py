46610f7b45421c5ec6fce45bb795b331a6afe636
Andrey Velikanov <andreyvelikanovv@gmail.com>
2021-04-19 01:28:42 +0300

    api v1 start.

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from rest_framework.response import Response
+
+from landing.models import *
+from .serializers import *
+
+# Create your views here.
+
+class FaqListView(generics.ListAPIView):
+    queryset = Faq.objects.all()
+    serializer_class = FaqSerializer
